d50f292ae372e43346ac5072f879273fe4f32c0d
Kleber <krodrigo@gmail.com>
2019-04-19 19:19:14 -0300

    Inclusão de componente exibir para receita

---
+++ src/Recipe.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Recipe = () => {
+  return (
+    <div>
+      <h1>Titulo</h1>
+      <p>Calorias</p>
+      <img src="" alt="" />
+    </div>
+  );
+};
+
+export default Recipe;
